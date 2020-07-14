023697d2284a6ae4ba85877f49bdef36cbc501e2
daviddicken <davidfromseattle@gmail.com>
2020-07-13 18:51:14 -0700

    testing weather

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const weatherArray = [];
 const express = require('express');
 const cors = require('cors');
+const { request, response } = require('express');
 require('dotenv').config();
 
 const app = express();
@@ -14,7 +16,6 @@ app.listen(PORT);
 // });
 
 app.get('/location', (request, response) => {
-  console.log
   try{
 
     let city = request.query.city;
@@ -29,6 +30,54 @@ app.get('/location', (request, response) => {
     response.status(500).send('There has been an error.. RUN!!!');
   }
 })
+
+//console.log('outside everything');
+let weatherData = require('./data/weather.json');
+
+weatherData.data.forEach(day => {
+  new Weather(day);
+});
+
+console.log(weatherArray);
+// const obj = new Weather(weatherData);
+
+// console.log('obj:',obj)
+// console.log('weatherdata:',weatherData);
+// console.log('[0]:',weatherData.data[0].weather.description);
+
+
+//==========================================
+app.get('/weather', (request, response) => {
+
+  let weatherData = require('./data/weather.json');
+
+  weatherData.data.forEach(day => {
+    new Weather(day);
+  });
+
+  response.send(weatherArray);
+
+  //const obj = new Weather(weatherData);
+  // [
+  //   {
+  //     "forecast": "Partly cloudy until afternoon.",
+  //     "time": "Mon Jan 01 2001"
+  //   },
+  //   {
+  //     "forecast": "Mostly cloudy in the morning.",
+  //     "time": "Tue Jan 02 2001"
+  //   },
+
+});
+
+function Weather(weatherInfo)
+{
+  this.forecast = weatherInfo.weather.description;
+  // this.forecast = weatherInfo.data[0].weather.description;
+  this.time = weatherInfo.valid_date;
+  // this.time = weatherInfo.data[0].valid_date;
+  weatherArray.push(this);
+}
 
 function Location(input, locData)
 {
